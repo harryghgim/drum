ea6e011bcf55fc6c7196d0de8153ce15055b7b26
harryghgim <harryghgim@gmail.com>
2021-04-19 23:44:10 +0900

    test passed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useRef, useEffect } from 'react'
+import './App.scss'
+
+function DrumPad(props) {
+  const audioRef = useRef(null);
+  const padId = `button-${props.k}`
+
+  const handleClick = (e) => {
+    audioRef.current.play()
+    document.getElementById('display').innerHTML = audioRef.current.textContent
+  }
+  
+  return (
+    <Button 
+      id={padId}
+      className="drum-pad mr-2" 
+      onClick={handleClick}
+      variant="secondary"
+      size="lg"
+    >
+      {props.k}
+      <audio 
+        className="clip" 
+        src={`audio/${props.k.toLowerCase()}.wav`}
+        id={props.k}
+        ref={audioRef}
+      >{props.description}
+      </audio>
+    </Button>
+  )
+}
 
 function App() {
+
+  const handleKeyDown = (e) => {
+
+    const k = e.key.toUpperCase()
+    const audioEl = document.getElementById(k)
+    
+    if (audioEl) {
+      const promise = audioEl.play()
+      if (promise !== undefined) {
+        promise
+          .then(_ => {
+            document.getElementById("display").innerHTML = audioEl.textContent
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+    } 
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown)
+  })
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Jumbotron id="drum-machine">
+      <Container id="pad-wrapper">
+        <div>
+          <DrumPad k="Q" description="Crash"/>
+          <DrumPad k="W" description="Hh Close"/>
+          <DrumPad k="E" description="Low Conga"/>
+        </div>
+        <div>
+          <DrumPad k="A" description="Bd"/> 
+          <DrumPad k="S" description="Clap"/> 
+          <DrumPad k="D" description="Ho"/> 
+        </div>
+        <div>
+          <DrumPad k="Z" description="Ma"/> 
+          <DrumPad k="X" description="Sd"/> 
+          <DrumPad k="C" description="Bongo"/>  
+        </div>
+      </Container>
+      <Container id="display">
+        Crash
+      </Container>
+    </Jumbotron>
+  )
 }
 
 export default App;
