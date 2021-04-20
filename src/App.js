d9b34a7c3f518bcbd52cfc122785ac4335f41c66
harryghgim <harryghgim@gmail.com>
2021-04-20 15:17:20 +0900

    visibility applied

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Button from 'react-bootstrap/Button'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useRef, useEffect } from 'react'
+import { useRef, useState } from 'react'
 import './App.scss'
 
 function DrumPad(props) {
@@ -34,33 +35,72 @@ function DrumPad(props) {
   )
 }
 
+function Message(props) {
+
+  const id = props.show ? 'show' : 'hide'
+  return (
+    <Alert variant="danger" id={id} className="message">
+      <Alert.Heading>qweasdzxc only</Alert.Heading>
+    </Alert>
+  )    
+
+}
+
 function App() {
+
+  const MAP = {
+    '81': 'Q',  
+    '87': 'W',
+    '69': 'E',
+    '65': 'A',
+    '83': 'S',
+    '68': 'D',
+    '90': 'Z',
+    '88': 'X',
+    '67': 'C'
+ }
+ 
+ const [show, setShow] = useState(false)
 
   const handleKeyDown = (e) => {
 
-    const k = e.key.toUpperCase()
-    const audioEl = document.getElementById(k)
+    const audioEl = document.getElementById(MAP[e.keyCode])
     
     if (audioEl) {
+      setShow(false)
       const promise = audioEl.play()
       if (promise !== undefined) {
         promise
           .then(_ => {
             document.getElementById("display").innerHTML = audioEl.textContent
+            audioEl.parentElement.style.backgroundColor = 'rgb(67, 87, 87)'
+            audioEl.parentElement.style.borderColor = 'rgb(67, 87, 87)'
           })
           .catch(error => {
             console.log(error)
           })
       }
     } 
+    else {      
+      setShow(true)
+    }
   }
 
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown)
-  })
-  
+  const handleKeyUp = (e) => {
+    const audioEl = document.getElementById(MAP[e.keyCode])
+    if (audioEl) {
+      const btnEl = audioEl.parentElement
+      btnEl.style.backgroundColor = "#6c757d"
+      btnEl.style.borderColor = "#6c757d"
+    }
+  }
 
+  window.onkeydown = handleKeyDown
+  window.onkeyup =  handleKeyUp
+  
   return (
+  <div>
+    <Message show={show}/>
     <Jumbotron id="drum-machine">
       <Container id="pad-wrapper">
         <div>
@@ -83,6 +123,7 @@ function App() {
         Crash
       </Container>
     </Jumbotron>
+  </div>
   )
 }
 
